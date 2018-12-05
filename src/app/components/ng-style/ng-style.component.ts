c941ec403b9800557482e49cf26968daa849cf5d
Rabindranath Ferreira <ferreiralox@gmail.com>
2018-12-04 22:38:36 -0300

    testing asyncronous response with efects in button

--- src/app/components/ng-style/ng-style.component.ts
+++ src/app/components/ng-style/ng-style.component.ts
@@ -8,14 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class NgStyleComponent implements OnInit {\
 
   sizeWords = 60;
-  addAlert: string = 'alert-danger';
+  addAlert = 'alert-danger';
   textProperties: Object = {
     style: true
   };
 
+  loading = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  asyncProcess() {
+    this.loading = true;
+
+    setTimeout( () => this.loading = false, 3000);
   }
 
 }
