cdd935f47f02384205ed02b1b97dfcfb27cfa35e
Rabindranath Ferreira <ferreiralox@gmail.com>
2018-12-04 21:56:33 -0300

    testing ngClass, ngStyle with data binding and anothers stuff

---
+++ src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style',
+  templateUrl: './ng-style.component.html',
+  styleUrls: ['./ng-style.component.css']
+})
+export class NgStyleComponent implements OnInit {\
+
+  sizeWords = 60;
+  addAlert: string = 'alert-danger';
+  textProperties: Object = {
+    style: false
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
