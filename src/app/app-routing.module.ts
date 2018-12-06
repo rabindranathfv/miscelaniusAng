d2d825cda1a3892319157385f4aa79ea40403999
Rabindranath Ferreira <ferreiralox@gmail.com>
2018-12-06 15:14:38 -0300

    using child routes with parameters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// components
+import { HomeComponent } from '../app/components/home/home.component';
+import { UserComponent } from '../app/components/user/user.component';
+import { NewUserComponent } from '../app/components/user/new-user/new-user.component';
+import { EditUserComponent } from '../app/components/user/edit-user/edit-user.component';
+import { DetailsUserComponent } from '../app/components/user/details-user/details-user.component';
+
+const routes: Routes = [
+  { path: 'home' , component: HomeComponent},
+  {
+    path: 'user/:id',
+    component: UserComponent,
+    children: [
+      { path: 'new-user', component: NewUserComponent},
+      { path: 'edit-user', component: EditUserComponent},
+      { path: 'details-user', component: DetailsUserComponent},
+      { path: '**', pathMatch: 'full', redirectTo: 'new-user'}
+    ]
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgStyleComponent } from './components/ng-style/ng-style.component';
 import { TitleDecoratorDirective } from './directives/title-decorator.directive';
+import { HomeComponent } from './components/home/home.component';
+import { UserComponent } from './components/user/user.component';
+import { EditUserComponent } from './components/user/edit-user/edit-user.component';
+import { NewUserComponent } from './components/user/new-user/new-user.component';
+import { DetailsUserComponent } from './components/user/details-user/details-user.component';
+import { NabvarComponent } from './components/nabvar/nabvar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgStyleComponent,
-    TitleDecoratorDirective
+    TitleDecoratorDirective,
+    HomeComponent,
+    UserComponent,
+    EditUserComponent,
+    NewUserComponent,
+    DetailsUserComponent,
+    NabvarComponent
   ],
   imports: [
     BrowserModule,
