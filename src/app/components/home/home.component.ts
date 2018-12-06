8b5e40be2a76a9d571b8f1eb74f68902b0d054e9
Rabindranath Ferreira <ferreiralox@gmail.com>
2018-12-06 16:12:19 -0300

    life Cicle hooks of components

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    console.log(`constructor`);
   }
 
+  ngOnInit() {
+    console.log(`ngOnInit`);
+  }
+  OnChanges() {
+    console.log(`ngOnChanges`);
+  }
+  ngDoCheck() {
+    console.log(`ngngDoCheck`);
+  }
+  ngAfterContentInit() {
+    console.log(`ngAfterContentInit`);
+  }
+  ngAfterContentChecked() {
+    console.log(`ngAfterContentChecked`);
+  }
+  ngAfterViewInit() {
+    console.log(`ngAfterViewInit`);
+  }
+  ngAfterViewChecked() {
+    console.log(`ngAfterViewChecked`);
+  }
+  ngOnDestroy() {
+    console.log(`ngOnDestroy`);
+  }
 }
